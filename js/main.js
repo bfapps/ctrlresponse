0d62ffdbe958df0f95fabd7ce7766658933a8869
Oscar <amirsardar9930@gmail.com>
2021-07-24 00:35:46 +0430

    final day mode

--- js/main.js
+++ js/main.js
@@ -3,22 +3,28 @@ localStorage.setItem('colorname1', "red");
 localStorage.setItem('colorname2', "blue");
 localStorage.setItem('colorname3', "green");
 localStorage.setItem('colorname4', "orange");
+localStorage.setItem('colorname5', "brown");
+localStorage.setItem('colorname6', "black");
+localStorage.setItem('colorname7', "purple");
 
 // setting colorVariables font-color
 localStorage.setItem('fontcolor1', "red");
 localStorage.setItem('fontcolor2', "blue");
 localStorage.setItem('fontcolor3', "green");
-localStorage.setItem('fontcolor4', "orange");
+localStorage.setItem('fontcolor4', "black");
+localStorage.setItem('fontcolor5', "brown");
+localStorage.setItem('fontcolor6', "orange");
+localStorage.setItem('fontcolor7', "purple");
 
 // Global Variables
 var fontColor="", colorName="";
 
 //random function
 function randomItems () {
-  let name,color, x= 1, y = 1;
+  let name,color, x=1, y=1;
   while ( x == y) {
-    x = Math.floor(Math.random() * 4) + 1;
-    y = Math.floor(Math.random() * 4) + 1;
+    x = Math.floor(Math.random() * 5) + 1;
+    y = Math.floor(Math.random() * 7) + 1;
   }
   name = "colorname" + x;
   color = "fontcolor" + y;
@@ -26,7 +32,15 @@ function randomItems () {
   fontColor = localStorage.getItem(color);
 }
 function makingDivs () {
-  randomItems();
-  alert(colorName + fontColor);
+  for (var i = 0; i < 24; i++) {
+    randomItems();
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    p.setAttribute('style', "color:" + fontColor + ";");
+    p.innerHTML = colorName;
+    div.appendChild(p);
+    document.getElementById('main-container').appendChild(div);
+  }
+
 }
 makingDivs ();
